1dc8728c7ad6efebd87c13469f25f289cf808bf5
Ali Zandieh <ali.zandieh@simplifyvms.com>
2026-06-12 16:50:21 +0100

    new updates

--- prerequisite/calico/install-in-kind.sh
+++ prerequisite/calico/install-in-kind.sh
@@ -1,3 +1,4 @@
-helm repo add projectcalico https://docs.tigera.io/calico/charts
-helm repo update
-helm install calico projectcalico/tigera-operator --namespace tigera-operator --create-namespace
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.32.0/manifests/v1_crd_projectcalico_org.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.32.0/manifests/tigera-operator.yaml
+curl -O https://raw.githubusercontent.com/projectcalico/calico/v3.32.0/manifests/custom-resources.yaml
+kubectl create -f custom-resources.yaml
